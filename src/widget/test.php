bb710d2b822cb8b2c954186c608da1c6b939c2cd
Barry Carlyon <barry@barrycarlyon.co.uk>
2013-10-06 10:18:01 +0100

    Best chuck in jquery to the test

--- src/widget/test.php
+++ src/widget/test.php
@@ -1,3 +1,5 @@
+<script src="//mysites/wordpress/wp-includes/js/jquery/jquery.js?ver=1.10.2"></script>
+
 <script src="//joind.local/widget/widget_attending.php"></script>
 <script>
     joindin_attending.draw(1);
